a460703e09c1a217023ffb110729da812d1154c4
Karel Babka <krlbabka@gmail.com>
2025-08-02 21:08:19 +0200

    Color swap

---
+++ Assets/_Game/Scripts/Gameplay/ColorSwap.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ColorSwap : MonoBehaviour
+{
+    private Camera _camera;
+
+    [SerializeField] private Color[] backgroundColors;
+    private bool _upperColor = true;
+
+    private void Awake() => _camera = GetComponent<Camera>();
+    private void OnEnable() => GameManager.Instance.OnPlayersSwapped += SwapCameraColor;
+    private void OnDisable() => GameManager.Instance.OnPlayersSwapped -= SwapCameraColor;
+    private void SwapCameraColor()
+    {
+        _camera.backgroundColor = _upperColor ? backgroundColors[1] : backgroundColors[0];
+        _upperColor = !_upperColor;
+    }
+}
